cd332fda648a3e9314117c1acaabb4d8b61ab2be
zhoupengzu@kanzhun.com <zhoupengzu@kanzhun.com>
2025-08-08 10:44:53 +0800

    init

---
+++ PZObjcSwiftUIMain/Classes/MainRootViewController.h
@@ -0,0 +1,16 @@
+//
+//  MainRootViewController.h
+//  PZObjectiveSwiftUI
+//
+//  Created by admin on 2025/8/7.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MainRootViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PZObjcSwiftUIMain/Classes/MainRootViewController.m
@@ -0,0 +1,32 @@
+//
+//  MainRootViewController.m
+//  PZObjectiveSwiftUI
+//
+//  Created by admin on 2025/8/7.
+//
+
+#import "MainRootViewController.h"
+
+@interface MainRootViewController ()
+
+@end
+
+@implementation MainRootViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = UIColor.greenColor;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
